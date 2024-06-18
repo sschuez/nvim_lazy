f88c7ab2811826d019061ada71aacad52f2907f8
Stephen <stephens@hey.com>
2024-06-18 09:30:19 +0200

    Reformat linters

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -1,15 +1,16 @@
 -- To make lazyvim faster, remove animation from mini.indentscope
 
 return {
-  -- {
-  --   "echasnovski/mini.nvim",
-  --   config = function()
-  --     require("mini.indentscope").setup({
-  --       options = { try_as_border = true },
-  --       draw = {
-  --         animation = require("mini.indentscope").gen_animation.none(),
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.indentscope").setup({
+        options = { try_as_border = true },
+        draw = {
+          animation = require("mini.indentscope").gen_animation.none(),
+        },
+        symbol = "╎",
+      })
+    end,
+  },
 }
